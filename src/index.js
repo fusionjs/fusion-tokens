95c8c9f649351b9e97a7627f86949883b794102c
Alexander Smith <amsmith@uber.com>
2018-02-01 15:32:19 -0800

    Leverage createToken from fusion-core and associated Flow types

--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
 // @flow
 
-// Helpers
-export const TokenType = {
-  Required: 0,
-  Optional: 1,
-};
-function Ref() {}
-class TokenImpl {
-  name: string;
-  ref: mixed;
-  type: $Values<typeof TokenType>;
-
-  constructor(name: string, ref: mixed) {
-    this.name = name;
-    this.ref = ref || new Ref();
-    this.type = ref ? TokenType.Optional : TokenType.Required;
-    if (!ref) {
-      // $FlowFixMe
-      this.optional = new TokenImpl(name, this.ref);
-    }
-  }
-}
-
-export type Token<T> = {
-  (): T,
-  optional: () => ?T,
-};
-export function createToken(name: string): Token<any> {
-  // $FlowFixMe
-  return new TokenImpl(name);
-}
+import {createToken} from 'fusion-core';
+import type {Token} from 'fusion-core';
 
 // Tokens
 type Fetch = (
